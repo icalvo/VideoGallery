a6e886d95f2691dcc4076d5fa1fe04bb195763e3
Ignacio Calvo <ignacio.calvo@gmail.com>
2025-11-16 13:05:07 +0100

    Load assemblies from directory as they are not loaded automatically if there is no using.

--- src/Library/DefaultPlugin/PluginLocator.cs
+++ src/Library/DefaultPlugin/PluginLocator.cs
@@ -1,14 +1,32 @@
-﻿using VideoGallery.Interfaces;
+﻿using System.Runtime.Loader;
+using VideoGallery.Interfaces;
 
 namespace VideoGallery.Library.DefaultPlugin;
 
 public static class PluginLocator
 {
-    public static Type? TryFindPluginType() =>
-        AppDomain.CurrentDomain.GetAssemblies()
-            .Where(a => a != typeof(PluginLocator).Assembly)
-            .SelectMany(a => a.GetTypes())
-            .FirstOrDefault(t =>
+    public static Type? TryFindPluginType()
+    {
+        var ctx = new AssemblyLoadContext("PluginLoader", isCollectible: true);
+        var thisTypeAssemblyLocation = typeof(PluginLocator).Assembly.Location;
+        var assemblyPath = 
+            Directory.GetFiles(Path.GetDirectoryName(thisTypeAssemblyLocation) ?? Environment.CurrentDirectory, "*.dll")
+            .FirstOrDefault(filePath =>
+            {
+                var asm = ctx.LoadFromAssemblyPath(filePath);
+                if (asm.Location == thisTypeAssemblyLocation)
+                    return false;
+                return asm.GetTypes()
+                .Any(t =>
+                    t is { IsClass: true, IsPublic: true } &&
+                    t.IsAssignableTo(typeof(ITagValidation)));
+            });
+        ctx.Unload();
+        if (assemblyPath is null) return null;
+        var asm = AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyPath);
+        return asm.GetTypes()
+            .First(t =>
                 t is { IsClass: true, IsPublic: true } &&
                 t.IsAssignableTo(typeof(ITagValidation)));
+    }
 }
